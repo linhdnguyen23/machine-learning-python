c266ed841df1a935ec1a626890d50ba78ee9a55c
linhdnguyen23 <ldn006@ucsd.edu>
2019-02-06 21:43:18 -0800

    Fix k-means sample code

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[3, 4]
+X = dataset.iloc[:, [3, 4]].values
 # y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
@@ -24,6 +24,7 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
+plt.close()
 wcss = []
 for i in range (1, 11):
     kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
@@ -31,24 +32,27 @@ for i in range (1, 11):
     wcss.append(kmeans.inertia_)
 plt.figure()
 plt.plot(range(1, 11), wcss)
-plt.title('The Elbow Method')
+plt.title('The Elbow method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
 
-# Fitting K-Means to the dataset
-kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
+# Applying k-means to the mall dataset
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
 y_kmeans = kmeans.fit_predict(X)
 
-# Visualising the clusters
-plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
-plt.title('Clusters of customers')
+# Visualizing the clusters
+plt.figure()
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', edgecolors = 'black', label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', edgecolors = 'black', label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', edgecolors = 'black', label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', edgecolors = 'black', label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', edgecolors = 'black', label = 'Cluster 5')
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids', edgecolors = 'black')
+plt.title('Clusters of clients')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
 plt.legend()
 plt.show()
+
