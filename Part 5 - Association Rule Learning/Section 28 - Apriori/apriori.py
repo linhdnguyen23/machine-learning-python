619482f886131ba7586bd50509c5e822863cb21b
linhdnguyen23 <ldn006@ucsd.edu>
2019-02-13 22:14:49 -0800

    fix apriori sample

--- Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -5,15 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Data Preprocessing
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
-transactions = []
-for i in range(0, 7501):
-    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+# Import the dataset
+dataset = pd.read_csv("Market_Basket_Optimisation.csv", header = None)
 
-# Training Apriori on the dataset
+transactions = []
+for i in range (0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range (0, 20)])
+    
+# Training apriori on the dataset
 from apyori import apriori
 rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
-# Visualising the results
+# Visualizing the results
 results = list(rules)
