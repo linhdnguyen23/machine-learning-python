c371f6ed95163c2b357ccaa1d53bfbaff10a4e8e
linhdnguyen23 <ldn006@ucsd.edu>
2019-02-21 20:46:08 -0800

    Small addition for easy view of the result list

--- Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -11,10 +11,18 @@ dataset = pd.read_csv("Market_Basket_Optimisation.csv", header = None)
 transactions = []
 for i in range (0, 7501):
     transactions.append([str(dataset.values[i, j]) for j in range (0, 20)])
-    
+ 
 # Training apriori on the dataset
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, 
+                min_lift = 3, min_length = 2)
 
 # Visualizing the results
 results = list(rules)
+
+   
+clean_results = []
+for i in range(0, len(results)):
+    clean_results.append('RULE:\t' + str(results[i][0]) + '\nSUPPORT:\t' + str(results[i][1]) + 
+                         '\nCONFIDENCE:\t' + str(results[i][2][0][2]) + '\nLIFT:\t' + 
+                         str(results[i][2][0][3]))
